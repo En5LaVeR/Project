e38f934580abaccc842f8292dbdc71ec5f4620b8
En5LaVeR <kavusspk@mail.ru>
2024-03-10 23:07:54 +1000

    10.03/Sea_battle 10.03/Start_calculators

---
+++ Test/Med_calculator.py
@@ -0,0 +1,22 @@
+class Selection_of_dosage:
+
+    def __init__(self, weight: float):
+        self.weight = weight
+
+    def medicines(self):
+        self.weight = float(input('Введите вес Собаки (в кг)'))
+        return self.ivermektin(), self.Alben()
+
+    def ivermektin(self):
+        return (
+            f"Ивермек = {0.02 * self.weight}мл;\nПроизводитель не рекомендует использовать раствор для инъекций для собак и кошек.\n"
+            f"Изначально он предназначался для свиней, лошадей, овец и крупного рогатого скота.")
+
+    def Alben(self):
+        return f'Alben = {10 * self.weight}мг'
+
+
+dog1 = Selection_of_dosage(weight=0)
+result = dog1.medicines()
+for a_drug in result:
+    print(a_drug)
